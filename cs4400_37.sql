6cc9c53efe5aa967d4b072773d08021f0a019c72
Brian Cai <briancai@gatech.edu>
2017-03-14 00:16:58 -0400

    working without enums finished bugfixes

--- cs4400_37.sql
+++ cs4400_37.sql
@@ -7,6 +7,7 @@ PRIMARY KEY (  `city` ,  `state` )
 */
 
 -- Location table
+-- works independently
 CREATE TABLE  `cs4400_37`.`LOCATION` (
 `city` VARCHAR( 100 ) NOT NULL ,
 `state` VARCHAR( 100 ) NOT NULL ,
@@ -14,28 +15,32 @@ CONSTRAINT PK_Location PRIMARY KEY (`city`,`state`)
 )
 
 -- Data Type
+-- works independetly
 CREATE TABLE  `cs4400_37`.`DATA_TYPE` (
 `type` VARCHAR( 100 ) NOT NULL ,
 PRIMARY KEY (`type`)
 )
 
 -- USER table
+-- TODO: error regarding usertype ENUM
 CREATE TABLE  `cs4400_37`.`USER` (
 `email` VARCHAR( 100 ) NOT NULL ,
 `username` VARCHAR( 100 ) NOT NULL ,
 `password` VARCHAR( 100 ) NOT NULL ,
+/*
 `usertype` ENUM(  `admin`,  `city scientist`,  `city official` ) NOT NULL ,
+*/
 PRIMARY KEY (  `email` ),
 UNIQUE ( `username` )
 )
 
 
 -- City Official
-CREATE TABLE  `cs4400_37`.`CITY OFFICIAL` (
+CREATE TABLE  `cs4400_37`.`CITY_OFFICIAL` (
 `email` VARCHAR( 100 ) NOT NULL ,
 `username` VARCHAR( 100 ) NOT NULL ,
 `title` VARCHAR( 100 ) NOT NULL ,
-`approved` BOOL NOT NULL ,
+`approved` BOOL FALSE ,
 `city` VARCHAR( 100 ) NOT NULL ,
 `state` VARCHAR( 100 ) NOT NULL ,
 PRIMARY KEY (  `email` ) ,
@@ -50,6 +55,7 @@ FOREIGN KEY (`city`, `state`)
     ON DELETE CASCADE
 )
 
+/*
 
 -- Admin 
 CREATE TABLE  `cs4400_37`.`ADMIN` (
@@ -67,16 +73,11 @@ PRIMARY KEY (  `email` ),
 UNIQUE ( `username` )
 )
 
--- City Official
-CREATE TABLE  `cs4400_37`.`CITY_OFFICIAL` (
-`email` VARCHAR( 100 ) NOT NULL ,
-`username` VARCHAR( 100 ) NOT NULL ,
-PRIMARY KEY (  `email` ),
-UNIQUE ( `username` )
-)
+*/
 
 
 -- Point of Interest
+-- works independently
 CREATE TABLE  `cs4400_37`.`POI` (
 `locationName` VARCHAR( 100 ) NOT NULL ,
 `zip` CHAR( 5 ) NOT NULL ,
@@ -94,19 +95,18 @@ FOREIGN KEY (`city`, `state`)
 CREATE TABLE  `cs4400_37`.`DATAPOINT` (
 `locationName` VARCHAR( 100 ) NOT NULL ,
 `dateTime` TIMESTAMP NOT NULL ,
-`type` VARCHAR NOT NULL ,
+`type` VARCHAR(100) NOT NULL ,
 `dataValue` INTEGER NOT NULL,
-`approved` BOOL NOT NULL ,
+`approved` BOOL FALSE ,
 PRIMARY KEY (  `locationName` ),
 UNIQUE ( `dateTime` )
-)
-FOREIGN KEY ('locationName')
+FOREIGN KEY (`locationName`)
     REFERENCES `POI` (`locationName`)
     ON DELETE CASCADE
-)
 FOREIGN KEY (`type`)
     REFERENCES `DATA_TYPE` (`type`)
     ON DELETE CASCADE
 )
+
 
 --ENGINE = MYISAM
