435fd69637355c7f15eab4436fa351ac8ec2cdf8
Brian Cai <briancai@gatech.edu>
2017-04-09 17:14:15 -0400

    front end stuff draft 1 no implementation of queries

---
+++ Front End/js/index.js
@@ -0,0 +1,14 @@
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+$('a').click( function() {alert("jquery test successful"); } );
+
+
+$('b').click( function() {alert("link to pending data points"); } );
+
+$('c').click( function() {alert("link to pending city official accounts"); } );
+
+$('d').click( function() {alert("logout button"); } );
+
