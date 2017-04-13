f77f5856c1ca038a4eca9be57ed6acd2d6fed7ba
Rohith Ravi <Rohith Ravi>
2017-04-12 22:15:57 -0400

    addPOI draft 2

--- Phase 3 SQL/addPOI.sql
+++ Phase 3 SQL/addPOI.sql
@@ -1,2 +1,5 @@
-INSERT INTO POI ("location_name", "zip", "flagged", "date_flagged", "city", "state")
+--if a city official clicks "add a new location"
+--city and state inputs are from the database and in drop-down format
+INSERT INTO POI
 VALUES ("name_input", "zip_input", "0", "NULL", "city_input", "state_input");
+--if location_name already exists: ERROR - "name_input already exists"
