7b3ab5ef787459d9a4db198d6127c4d7b291eb28
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-12 21:43:43 -0400

    added some logic to login

--- Phase 3 SQL/login.sql
+++ Phase 3 SQL/login.sql
@@ -1,1 +1,6 @@
-select (email,username,user_type) from USER where( USER.email = input1 and USER.password = input2)
+select email,username,user_type from USER where( USER.email = input1 and USER.password = input2)
+-- select username, password,user_type from USER where( USER.username = "brohith" and USER.password = "password")
+-- if empty set:
+--      error "Invalid username or password"
+-- else:
+--      successful login use user_type to direct user to correct page
