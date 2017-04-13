f9038b31b1953ee52dff01de0dbe70a306145fde
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-12 22:05:31 -0400

    more stuff lmao

--- Phase 3 SQL/login.sql
+++ Phase 3 SQL/login.sql
@@ -1,4 +1,4 @@
-select email,username,user_type from USER where( USER.email = input1 and USER.password = input2)
+select email,username,user_type from USER where( USER.username = un_input  and USER.password = pw_input)
 -- select username, password,user_type from USER where( USER.username = "brohith" and USER.password = "password")
 -- if empty set:
 --      error "Invalid username or password"
