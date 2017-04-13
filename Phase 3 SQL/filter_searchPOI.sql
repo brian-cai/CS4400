25607f8d9ad5b9f3676d0f57b846edc6033af8cb
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-13 00:27:18 -0400

    fleshed out the filter/search POI

--- Phase 3 SQL/filter_searchPOI.sql
+++ Phase 3 SQL/filter_searchPOI.sql
@@ -1,9 +1,41 @@
 -- this is a pretty complicated one
 
+
+-- populate drop downs with this data
+-- ********* All of these must have an option for null when there is no value selected
+select location_name from POI;
+select city from LOCATION;
+select state from LOCATION;
+
+
+
+
 select * from POI where
     (name_input IS NULL OR location_name = name_input) and
     (city_input IS NULL OR city = city_input) and
     (state_input IS NULL OR state = state_input) and
     (zip_input IS NULL OR zip = zip_input) and
     (flagged_input IS NULL OR flagged = flagged_input) and
-    (dflag_input IS NULL OR date_flagged = dflag_input) and
+    (dflag_input IS NULL OR date_flagged = dflag_input);
+
+
+
+
+-- test queries for dummy data on the php server
+
+-- select * from POI where
+--     (null IS NULL OR location_name = null) and
+--     ('Atlanta' IS NULL OR city = 'Atlanta') and
+--     (null IS NULL OR state = null) and
+--     (30333 IS NULL OR zip = 30333) and
+--     (null IS NULL OR flagged = null) and
+--     (null IS NULL OR date_flagged = null)
+
+
+-- select * from POI where
+--     (null IS NULL OR location_name = null) and
+--     (null IS NULL OR city = null) and
+--     (null IS NULL OR state = null) and
+--     (null IS NULL OR zip = null) and
+--     (null IS NULL OR flagged = null) and
+--     (null IS NULL OR date_flagged = null)
