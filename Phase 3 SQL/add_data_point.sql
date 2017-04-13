99e906087c716aa0b9ec888cdf61791b97091954
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-12 23:06:32 -0400

    adding semi-colons and add_data_point

---
+++ Phase 3 SQL/add_data_point.sql
@@ -0,0 +1,8 @@
+-- populates POI location name dropdown
+select location_name from POI;
+
+-- populates the data_type dropdown
+select type from DATA_TYPE;
+
+insert into DATA_POINT values(name_input, datetime_input, type_inout, value_input, 0);
+
--- Phase 3 SQL/register.sql
+++ Phase 3 SQL/register.sql
@@ -1,7 +1,12 @@
-insert into USER values(em_input, un_input, pw_input, type_input)
+insert into USER values(em_input, un_input, pw_input, type_input);
 --
 -- if confpw_input != pw_input: error "passwords don't match "
 
+
 -- if type_input == "City Official":
-insert into CITY_OFFICIAL values(un_input, title_input, 0, city_input, state_input)
+-- populate city and state dropdowns
+    select city from LOCATION;
+    select state from LOCATION;
+
+insert into CITY_OFFICIAL values(un_input, title_input, 0, city_input, state_input);
 -- assumption: city and state are already in the database
