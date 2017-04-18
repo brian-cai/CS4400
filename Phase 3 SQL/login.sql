947a76d5b882f76992903b261443696ebafc0eed
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-17 23:44:43 -0400

    POI Detail

--- Phase 3 SQL/login.sql
+++ Phase 3 SQL/login.sql
@@ -1,6 +1,10 @@
 select email,username,user_type from USER where( USER.username = un_input  and USER.password = pw_input);
--- select username, password,user_type from USER where( USER.username = "brohith" and USER.password = "password")
+
 -- if empty set:
 --      error "Invalid username or password"
 -- else:
 --      successful login use user_type to direct user to correct menu page
+
+
+-- TEST
+-- select username, password,user_type from USER where( USER.username = "Arnold" and USER.password = "Strong")
