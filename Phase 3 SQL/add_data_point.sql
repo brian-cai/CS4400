10a4ca16f35b8fd7419c91be853b9bd5cb48bacf
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-18 02:24:37 -0400

    populated a/r datapoints

--- Phase 3 SQL/add_data_point.sql
+++ Phase 3 SQL/add_data_point.sql
@@ -6,3 +6,10 @@ select type from DATA_TYPE;
 
 insert into DATA_POINT values(name_input, datetime_input, type_inout, value_input, 0);
 
+
+
+
+-- populate drop downs
+
+select city from LOCATION
+select state from LOCATION
