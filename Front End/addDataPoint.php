ef8227397d6d04ce94d9207a1dfea6dcd9231195
Rohith Ravi <Rohith Ravi>
2017-04-21 01:50:21 -0400

    add data point is working

--- Front End/addDataPoint.php
+++ Front End/addDataPoint.php
@@ -10,16 +10,16 @@ if ($mysqli->connect_errno) {
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>CS 4400 - Add Data Point</title>  
+  <title>CS 4400 - Add Data Point</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 
 <!-- SQL QUERIES for location dropdown-->
-<?php 
+<?php
 $sql = "select location_name from POI;";
 
 if (!$result = $mysqli->query($sql)) {
-    // Oh no! The query failed. 
+    // Oh no! The query failed.
     echo "Sorry, the website is experiencing problems.";
 
     // Again, do not do this on a public site, but we'll show you how
@@ -37,9 +37,9 @@ $count=$result->num_rows;
 <body>
   <div class="form">
 
-    <form action = "addDataPoint.php" method = "get">
+    <form action = "addDataPoint.php" method = "post">
       <h1>ADD NEW DATA POINT </h1>
-      POI location name 
+      POI location name
       <select name="locationname">
           <?php
             $i = 0;
@@ -48,12 +48,12 @@ $count=$result->num_rows;
               //put queries from database here
               if($row=$result->fetch_assoc()) {
                  $loc = $row['location_name'];
-              }          
+              }
             ?>
-          
-              <option value= "$loc" > <?php echo $loc ?></option>
-          
-          
+
+              <option value= "<?php echo $loc ?>"> <?php echo $loc ?></option>
+
+
 
           <?php
           $i++;
@@ -70,11 +70,11 @@ $count=$result->num_rows;
       Date and Time: <input type="datetime-local" name="time">
       <br><br>
 <!-- SQL QUERIES for data type dropdown-->
-<?php 
+<?php
 $sql = "select type from DATA_TYPE;";
 
 if (!$result = $mysqli->query($sql)) {
-    // Oh no! The query failed. 
+    // Oh no! The query failed.
     echo "Sorry, the website is experiencing problems.";
 
     // Again, do not do this on a public site, but we'll show you how
@@ -97,12 +97,12 @@ $count=$result->num_rows;
               //put queries from database here
               if($row=$result->fetch_assoc()) {
                  $type = $row['type'];
-              }          
+              }
             ?>
-          
-              <option value= "$type" > <?php echo $type ?></option>
-          
-          
+
+              <option value= "<?php echo $type ?>" > <?php echo $type ?></option>
+
+
 
           <?php
           $i++;
@@ -111,25 +111,47 @@ $count=$result->num_rows;
         </select>
         <br><br>
       <input type="number" name="datavalue" placeholder="data value"/>
-      
+
       <button>
         <a href="index.php">
-          Log Out 
-        </a>        
-      </button>
-      <button input type="submit">
-        <a href="#">
-          Submit
+          Log Out
         </a>
+      </button>
+      <button href="#" input type="submit">
+
+          Submit
+
       </button>
     </form>
 
   </div>
-POI location: <?php echo $_GET["locationname"]; ?> <br>
-Data Type: <?php echo $_GET["datatype"]; ?> <br> 
-Time: <?php echo $_GET["time"]; ?> <br> 
-Data Value: <?php echo $_GET["datavalue"]; ?> <br>
+POI location: <?php echo $_POST["locationname"]; ?> <br>
+Data Type: <?php echo $_POST["datatype"]; ?> <br>
+Time: <?php echo $_POST["time"]; ?> <br>
+Data Value: <?php echo $_POST["datavalue"]; ?> <br>
 
+<?php $locc = $_POST["locationname"]; ?>
+<?php $datatype = $_POST["datatype"]; ?>
+<?php $time = $_POST["time"]; ?>
+<?php $value = $_POST["datavalue"]; ?>
+Query happens below
+<?php
+$sql = "INSERT INTO DATA_POINT VALUES( '$locc', '$time', '$datatype', '$value', NULL );";
+echo "$sql";
+
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed.
+    echo "Sorry, the website is experiencing problems.";
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: <br>";
+    echo "Query: " . $sql . "<br>";
+    echo "Errno: " . $mysqli->errno . "<br>";
+    echo "Error: " . $mysqli->error . "<br>";
+    exit;
+}
+?>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
 <script src="js/index.js"></script>
