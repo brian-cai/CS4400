03d9ec1f50b25891614cfb4c743d5bf23c019230
Victor Rohlfs <victorrohlfs@lawn-128-61-114-239.lawn.gatech.edu>
2017-04-20 21:46:07 -0400

    city official login constraint update

--- Phase 3 SQL/login.sql
+++ Phase 3 SQL/login.sql
@@ -11,3 +11,12 @@ select email,username,user_type from USER where( USER.username = un_input  and U
 -- TEST
 -- select username, password,user_type from USER where( USER.username = "Arnold" and USER.password = "Strong")
 
+
+
+---Citi_Official constraint-------
+SELECT approved 
+FROM CITY_OFFICIAL
+WHERE username = un_input
+## code will have to handle the error. 
+## If approved == NULL (i.e city_official is pending)
+##		deny access to the application (i.e show error)
