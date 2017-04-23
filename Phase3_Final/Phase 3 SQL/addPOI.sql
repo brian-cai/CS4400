77042b6f60af2c69dc40b13caa0a012c64c1e666
Jose A. Berlingeri <Jose A. Berlingeri>
2017-04-23 14:00:24 -0400

    deleting stuff not needed for final submission

--- Phase3_Final/Phase 3 SQL/addPOI.sql
+++
@@ -1,9 +0,0 @@
---if a city official clicks "add a new location"
---city and state inputs are from the database and in drop-down format
-INSERT INTO POI
-VALUES (name_input, zip_input, 0, NULL, city_input, state_input);
---if location_name already exists: ERROR - "name_input already exists"
-
-
-INSERT INTO POI
-VALUES ("Bev-hills", 90210, 0, NULL, "Los Angeles", "CA");
